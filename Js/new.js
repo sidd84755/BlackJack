7a39744a6ac44b0b03229c9d7019083b29a6d3c0
Siddharth Chaturvedi <sidd84755@gmail.com>
2023-02-22 23:20:28 +0530

    added features

--- Js/new.js
+++ Js/new.js
@@ -3,16 +3,23 @@ let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 let firstCard = 10
 let secondCard = 11
+let cards = [firstCard,secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
+let sent = ["hello" ,"my name","is","siddharth"]
+let prEl = document.querySelector("#pr-el")
+
+for(i = 0; i < sent.length; i++){
+    prEl.textContent += sent[i] + " "
+}
 function startGame(){
     renderGame()
 }
 function renderGame(){
     sumEl.textContent = "Sum: " + sum
-    cardsEl.textContent = "Cards: " + firstCard +" "+ secondCard
+    cardsEl.textContent = "Cards: " + cards[0] +" "+ cards[1]
     if (sum <= 20) {
         message = "Do you want to draw a new card? 🙂"
     } else if (sum === 21) {
@@ -28,6 +35,7 @@ function newCard(){
     console.log("Drawing a new card from the deck!")
     let nCard = 6
     sum += nCard
+    cards.push(nCard)
     renderGame()
 }
 
